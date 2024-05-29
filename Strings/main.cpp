7730f1ef87f1a0677419af2a9cdf20633616061b
YocoZuna <111345729+YocoZuna@users.noreply.github.com>
2024-05-29 11:17:15 +0200

    update
    update

--- Strings/main.cpp
+++ Strings/main.cpp
@@ -19,5 +19,20 @@ int main(void)
     std::cout<<s4<<std::endl;
     std::cout<<s5<<std::endl;
     std::cout<<s6<<std::endl;
-    std::cout<<s8<<std::endl;
+    std::cout<<s8<<std::endl;   
+    std::string sh = "Helloworld";
+    std::string concat  = sh+s2+s3; // String concat in C++
+    //std:: string concat = "Elo" + "Siema" ; // This is forbiden 
+    concat.at(10) = 'X';
+    std::cout<<concat<<std::endl;
+    std::string s69 = concat.substr(3,3);  // (Inex,lenght)
+    concat += sh;
+    std::cout<<concat.find("Helloworld")<<std::endl; // return index if not find return string::npos
+    std::cout<<concat.find("Helloworld",3)<<std::endl; // return index of first occurenc starting for index 3
+    std::cout<<s69; 
+    
+   
+    std::cout<<concat.erase(5,8)<<std::endl; // Delates chars form given postion to length
+    concat.clear(); // clears whole var
+    std::cout<<concat<<std::endl;
 }
